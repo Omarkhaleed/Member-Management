fcc0505d4db1ed9646f2fe0f00fbd58e2bb26406
Omar khalid <86879244+Omarkhaleed@users.noreply.github.com>
2022-04-19 18:04:47 +0200

    Add files via upload

---
+++ Controllers/MemberController.cs
@@ -0,0 +1,135 @@
+﻿using FirstTask.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FirstTask.Models;
+using System.Data.Entity;
+using System.IO;
+
+namespace FirstTask.Controllers
+{
+    public class MemberController : Controller
+    {
+        CompanyContext _context = new CompanyContext();
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult ShowMembers(string sortOrder)
+        {
+           
+            var members = from s in _context.Members
+                          select s;
+
+
+            switch (sortOrder)
+            {
+                case "ASC":
+                  members = members.OrderBy(s => s.FirstName);
+                    break;
+                case "DESC":
+                members = members.OrderByDescending(s => s.FirstName);
+                    break;
+                case "desc":
+                 members= members.OrderByDescending(s => s.MemberId);
+                    break;
+                default:
+                    members = members.OrderBy(s => s.MemberId);
+                    break;
+            }
+            return View(members.ToList());
+        }
+
+
+        [HttpGet]
+        public ActionResult FindMembers(int id)
+        {
+            
+            var member= _context.Members.Where(pp=>pp.MemberId.Equals(id)).ToList();
+            return View(member);
+        }
+        [HttpGet]
+        public ActionResult FindMembers2(string name)
+        {
+            var member = _context.Members.Where(pp => pp.FirstName.Contains(name)).ToList();
+            return View(member);
+        }
+
+        [HttpGet]
+        public ActionResult AddMember()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+
+        public ActionResult AddMember(Member member, HttpPostedFileBase File)
+
+        {
+            string name = Path.GetFileNameWithoutExtension(File.FileName);
+            string extention = Path.GetExtension(File.FileName);
+            name = name + DateTime.Now.ToString("yymmssfff") + extention;
+
+            File.SaveAs(Server.MapPath($"~/Resources/{name}"));
+           
+            member.File = name;
+           
+
+     
+
+       
+
+            if (ModelState.IsValid == false) { return View(); }
+
+            _context.Members.Add(member);
+            _context.SaveChanges();
+            return RedirectToAction("ShowMembers");
+        }
+        [HttpGet]
+        public ActionResult EditMember(int id)
+        {
+            var member = _context.Members.Find(id);
+            return View(member);
+        }
+        [HttpPost]
+        public ActionResult EditMember(Member member, HttpPostedFileBase File)
+        {
+            string name = Path.GetFileNameWithoutExtension(File.FileName);
+            string extention = Path.GetExtension(File.FileName);
+            name = name + DateTime.Now.ToString("yymmssfff") + extention;
+
+            File.SaveAs(Server.MapPath($"~/Resources/{name}"));
+          
+            member.File = name;
+
+            if (ModelState.IsValid == false)
+            {
+
+                return View();
+            }
+         
+            _context.Entry(member).State = EntityState.Modified;
+            _context.SaveChanges();
+            return RedirectToAction("ShowMembers");
+        }
+
+        [HttpGet]
+        public ActionResult DeleteMember(int id)
+        {
+
+
+            var delete = _context.Members.Find(id);
+            _context.Members.Remove(delete);
+            _context.SaveChanges();
+            return RedirectToAction("ShowMembers");
+
+        }
+    }
+}
---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using FirstTask.Models;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web;
+using System.Data.Entity;
+using System.IO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstTask.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        CompanyContext _context = new CompanyContext();
+
+      
+
+     // Get Member with name or all members
+
+        public  IEnumerable<Member> Get(string name)
+        {
+            if (name == null)
+            {
+                var s = _context.Members.ToList();
+                return s;
+            }
+            else
+            {
+               var ss = _context.Members.Where(pp => pp.FirstName.Contains(name));
+
+                return ss.ToList();
+            }
+
+
+        }
+
+        // Get Member With Id
+        [System.Web.Mvc.HttpGet]
+        public IEnumerable<string> mem(int id)
+        {
+            yield return "your dats is";
+        }
+        
+        // For Add Member
+        [System.Web.Mvc.HttpPost]
+       
+        public HttpResponseMessage Post( Member data)
+        {
+
+            //string name = Path.GetFileNameWithoutExtension(File.FileName);
+            //string extention = Path.GetExtension(File.FileName);
+            //name = name + DateTime.Now.ToString("yymmssfff") + extention;
+
+            //File.SaveAs($"D:/WebDevelpment/ITI(.NET)/code/FirstTask/Resources/{name}");
+
+            //data.File = name;
+            _context.Members.Add(data);
+            _context.SaveChanges();
+            return Request.CreateResponse(HttpStatusCode.OK, "Member added");
+
+        }
+
+       // For Edit Member
+
+       [System.Web.Mvc.HttpPut]
+        public HttpResponseMessage Put([System.Web.Http.FromBody] Member data)
+        {
+            var edit = _context.Members.Find(data.MemberId);
+
+            edit.FirstName = data.FirstName;
+            edit.SocilID = data.SocilID;
+            edit.File = data.File;
+             _context.SaveChanges();
+            return Request.CreateResponse(HttpStatusCode.OK, "Member edited");
+        }
+
+      
+        // For delete Member
+
+        [System.Web.Mvc.HttpPost]
+
+        public HttpResponseMessage post( int MemberId)
+        {
+          
+            try
+               {
+
+                    var delete = _context.Members.Find(MemberId);
+
+                     if (delete == null)
+                       {
+                    return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Member with id = " + MemberId.ToString() + " not found");
+                       }
+                else 
+                {
+                    _context.Members.Remove(delete);
+                    _context.SaveChanges();
+
+
+                    return Request.CreateResponse(HttpStatusCode.OK, "Member deleted");
+                }
+
+            }
+            catch (Exception Ex)
+              {
+
+                       return Request.CreateErrorResponse(HttpStatusCode.BadRequest, Ex);
+                   }
+            }
+        
+        }
+}
