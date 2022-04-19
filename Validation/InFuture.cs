4413b6ae373f7bb294225c2b075bfb59fccb4b19
Omar khalid <86879244+Omarkhaleed@users.noreply.github.com>
2022-04-19 18:05:55 +0200

    Add files via upload

---
+++ Validation/InFuture.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TryValidation.Validation
+{
+    public class InFuture:ValidationAttribute
+    {
+        //public override bool IsValid(object value)
+        //{
+        //    //return base.IsValid(value);
+        //}
+    }
+}
